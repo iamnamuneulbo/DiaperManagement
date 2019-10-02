ba175f0f698256ced58acdb74904584b6be22372
leesk9719 <leesk9719@gmail.com>
2019-10-03 04:44:33 +0900

    수정, 삭제 기능/DAO 수정

--- hanium/src/device/DeviceInfo.java
+++ hanium/src/device/DeviceInfo.java
@@ -2,11 +2,18 @@ package device;
 
 public class DeviceInfo {
 	private String deviceID;
-	
+	private String checkDate;
+
 	public String getDeviceID() {
 		return deviceID;
 	}
 	public void setDeviceID(String deviceID) {
 		this.deviceID = deviceID;
+	}
+	public String getCheckDate() {
+		return checkDate;
+	}
+	public void setCheckDate(String checkDate) {
+		this.checkDate = checkDate;
 	}
 }
